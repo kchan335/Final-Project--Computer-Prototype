a516799d7233b2363b8603b145db71e4506a1c79
Kai-Ying Chang <35711975+kchan335@users.noreply.github.com>
2018-04-12 00:17:46 -0400

    This is an initial prototype

---
+++ README.md
@@ -0,0 +1,12 @@
+The content of the prototype:
+1. The timer and the record of best time are incompletely implemented.
+2. There are total 5 levels for the game, but now the prototype only shows the level 1.
+3. When the words match with the pictures, they will disappear together. But now I'm still 
+figuring out how to do that.
+
+The task for the prototype:
+1. Read the instruction on the homepage, and click the button to begin the game.
+2. Read the instruction on the gamepage, and follow the instruction.
+
+Access the my prototype:
+http://mysite.pratt.edu/~kchan335/
---
+++ my script.js
@@ -0,0 +1,65 @@
+/* draggable boxes */
+
+$( function() {
+  $( "#draggable" ).draggable();
+} );
+
+$( function() {
+  $( "#draggable2" ).draggable();
+} );
+
+$( function() {
+  $( "#draggable3" ).draggable();
+} );
+
+$( function() {
+  $( "#draggable4" ).draggable();
+} );
+
+$( function() {
+  $( "#draggable5" ).draggable();
+} );
+
+$( function() {
+  $( "#draggable6" ).draggable();
+} );
+
+$( function() {
+  $( "#draggable7" ).draggable();
+} );
+
+$( function() {
+  $( "#draggable8" ).draggable();
+} );
+
+$( function() {
+  $( "#draggablepicture" ).draggable();
+} );
+
+$( function() {
+  $( "#draggablepicture2" ).draggable();
+} );
+
+$( function() {
+  $( "#draggablepicture3" ).draggable();
+} );
+
+$( function() {
+  $( "#draggablepicture4" ).draggable();
+} );
+
+$( function() {
+  $( "#draggablepicture5" ).draggable();
+} );
+
+$( function() {
+  $( "#draggablepicture6" ).draggable();
+} );
+
+$( function() {
+  $( "#draggablepicture7" ).draggable();
+} );
+
+$( function() {
+  $( "#draggablepicture8" ).draggable();
+} );
