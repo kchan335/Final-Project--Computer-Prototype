e0654cb4aba08900ed1fd12f0ef7d23b26584cbc
kchan335 <35711975+kchan335@users.noreply.github.com>
2018-05-02 06:00:16 -0400

    Final project update
    1. Add "About"page and "Contact" page.
    2. Update README.md

--- README.md
+++ README.md
@@ -10,3 +10,52 @@ The task for the prototype:
 
 Access the my prototype:
 http://mysite.pratt.edu/~kchan335/
+
+------------------------------------------------------------------------------------------
+
+Changes:
+1. Add "About"page and "Contact" page.
+2. Instead of 5 levels for a game, I changed it to just 1 level for each game.
+3. Also, instead of the record of the best time, I changed to record the last time by using cookie.
+
+
+The tasks for the new changes:
+
+You are a foreigner and would like to enhance the English ability.
+You decide to use the website to learn the basic everyday English around you. 
+
+Goal: 1. Play the game "classroom" as fast as you can
+Subgoals:
+1.1 Hover over the "classroom" game on homepage
+1.2 Read the instruction on the gamepage
+1.3 Match the words and pictures
+1.4 After finishing, a pop-up window will show up
+1.5 Click the "cancel" on the pop-up window
+1.6 Scroll down to click the button "Explore other games"
+1.7 Go back the home page
+
+Goal: 2. Contact the author of the website.
+Subgoals:
+1.1 Find the link "Contact"
+1.2 Click the link
+1.3 Fill out your name and type your comments, but do not fill out your email
+1.4 Click the button "submit"
+
+Goal: 3. Understand the purpose of the website.
+Subgoals:
+1.1 Read the introduction on the homepage
+1.2 Find the link "About"
+1.3 Read the content
+
+Now there are three topics of the games on the homepage, but I only finished the first topic.
+If I have more time, I'll finish them all because their functions are the same. 
+
+
+
+
+
+
+
+
+
+
--- my script.js
+++ my script.js
@@ -1,65 +1,224 @@
-/* draggable boxes */
+/* draggble texts */
+var display;
+$( function() {
+  $( "#draggableLaptopComputerText" ).draggable( {
+    revert: true
+  });
+});
 
 $( function() {
-  $( "#draggable" ).draggable();
-} );
+  $( "#draggableStaplerText" ).draggable( {
+    revert: true
+  });
+});
 
 $( function() {
-  $( "#draggable2" ).draggable();
-} );
+  $( "#draggablePaperClipText" ).draggable( {
+    revert: true
+  });
+});
 
 $( function() {
-  $( "#draggable3" ).draggable();
-} );
+  $( "#draggableDiaryText" ).draggable( {
+    revert: true
+  });
+});
 
 $( function() {
-  $( "#draggable4" ).draggable();
-} );
+  $( "#draggableHighlighterText" ).draggable( {
+    revert: true
+  });
+});
 
 $( function() {
-  $( "#draggable5" ).draggable();
-} );
+  $( "#draggableFolderText" ).draggable( {
+    revert: true
+  });
+});
 
 $( function() {
-  $( "#draggable6" ).draggable();
-} );
+  $( "#draggablePunchText" ).draggable( {
+    revert: true
+  });
+});
 
 $( function() {
-  $( "#draggable7" ).draggable();
-} );
+  $( "#draggableNotepadText" ).draggable( {
+    revert: true
+  });
+});
+
+
+// jQuery that allows elements to be dropped
+$( function() {
+$( "#droppableStaplerPicture" ).droppable( {
+  // we specify what happens when something is dropped
+ // https://api.jqueryui.com/droppable/#event-drop
+  drop: function (event, ui) {
+    // store the ID of the source element that was dropped
+    var id = ui.draggable.attr("id");
+    // make sure it's the correct corresponding text
+    if (id === 'draggableStaplerText') {
+      // count correct number
+      increaseNumberCorrect();
+      // hide the stapler picture
+      $(this).hide();
+      // hide the stapler text
+      $("#draggableStaplerText").hide();
+    }
+  }
+});
+});
+//
+$( function() {
+$( "#draggableLaptopComputerPicture" ).droppable( {
+  drop: function (event, ui) {
+    var id = ui.draggable.attr("id");
+    if (id === 'draggableLaptopComputerText') {
+      increaseNumberCorrect();
+      $(this).hide();
+      $("#draggableLaptopComputerText").hide();
+    }
+  }
+});
+});
 
 $( function() {
-  $( "#draggable8" ).draggable();
-} );
+$( "#draggablePaperClipPicture" ).droppable( {
+  drop: function (event, ui) {
+    var id = ui.draggable.attr("id");
+    if (id === 'draggablePaperClipText') {
+      increaseNumberCorrect();
+      $(this).hide();
+      $("#draggablePaperClipText").hide();
+    }
+  }
+});
+});
 
 $( function() {
-  $( "#draggablepicture" ).draggable();
-} );
+$( "#draggableDiaryPicture" ).droppable( {
+  drop: function (event, ui) {
+    var id = ui.draggable.attr("id");
+    if (id === 'draggableDiaryText') {
+      increaseNumberCorrect();
+      $(this).hide();
+      $("#draggableDiaryText").hide();
+    }
+  }
+});
+});
 
 $( function() {
-  $( "#draggablepicture2" ).draggable();
-} );
+$( "#draggableHighlighterPicture" ).droppable( {
+  drop: function (event, ui) {
+    var id = ui.draggable.attr("id");
+    if (id === 'draggableHighlighterText') {
+      increaseNumberCorrect();
+      $(this).hide();
+      $("#draggableHighlighterText").hide();
+    }
+  }
+});
+});
 
 $( function() {
-  $( "#draggablepicture3" ).draggable();
-} );
+$( "#draggableFolderPicture" ).droppable( {
+  drop: function (event, ui) {
+    var id = ui.draggable.attr("id");
+    if (id === 'draggableFolderText') {
+      increaseNumberCorrect();
+      $(this).hide();
+      $("#draggableFolderText").hide();
+    }
+  }
+});
+});
 
 $( function() {
-  $( "#draggablepicture4" ).draggable();
-} );
+$( "#draggablePunchPicture" ).droppable( {
+  drop: function (event, ui) {
+    var id = ui.draggable.attr("id");
+    if (id === 'draggablePunchText') {
+      increaseNumberCorrect();
+      $(this).hide();
+      $("#draggablePunchText").hide();
+    }
+  }
+});
+});
 
 $( function() {
-  $( "#draggablepicture5" ).draggable();
-} );
+$( "#draggableNotepadPicture" ).droppable( {
+  drop: function (event, ui) {
+    var id = ui.draggable.attr("id");
+    if (id === 'draggableNotepadText'){
+      increaseNumberCorrect();
+      $(this).hide();
+      $("#draggableNotepadText").hide();
+    }
+  }
+});
+});
 
-$( function() {
-  $( "#draggablepicture6" ).draggable();
-} );
+//correct answers
+var numberCorrect = 0;
+function increaseNumberCorrect (){
+    numberCorrect++;
+    if (numberCorrect == 8){
+        setCookie ("bestTime", display.textContent);
+        console.log (getCookie("bestTime"));
 
-$( function() {
-  $( "#draggablepicture7" ).draggable();
-} );
 
-$( function() {
-  $( "#draggablepicture8" ).draggable();
-} );
+        var r = confirm("Congratulation! You finished the game!");
+        if (r == true){
+          window.open("Homepage.html");
+    }
+
+  }
+ }
+
+ function setCookie(name,value){
+  var expires = "; expires=" + Date.now() + 100000;
+  document.cookie = name + "=" + (value || "") + expires+ "; path=/";
+}
+
+ function getCookie(name){
+   var nameEQ = name + "=";
+   var ca = document.cookie.split(';');
+   for(var i=0; i< ca.length ; i++){
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if(c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+   }
+   return null;
+ }
+
+ function eraseCookie(name){
+   document.cookie = name+ '=; Max-Age=-99999999;';
+ }
+
+//Timer
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10)
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    document.getElementById("lastTime").innerHTML = getCookie("bestTime");
+    var fiveMinutes = 60 * 2;
+    display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
